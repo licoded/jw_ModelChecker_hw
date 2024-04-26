6cde98678b069fe652f836d0978a2f3c6bef04f3
licoded <busy.li@foxmail.com>
2024-04-27 07:54:56 +0800

    stash long before, now date is Apr.27th

--- test.cpp
+++ test.cpp
@@ -1,118 +1,159 @@
 #include <iostream>
+#include <cstring>
 #include <unordered_map>
-extern "C" {
-    #include "aiger.h"
+extern "C"
+{
+#include "aiger.h"
 }
 #include "formula/af_utils.h"
+#define BOUND_LEN 20
 using namespace aalta;
 
-int main() {
-    const char* aigerFile = "example.aag";
+enum class CIRCUIT_TYPE
+{
+    LATCHE,
+    OUTPUT,
+};
+
+unsigned get_lit(aiger *circuit, CIRCUIT_TYPE type, unsigned offset)
+{
+    switch (type)
+    {
+    case CIRCUIT_TYPE::LATCHE:
+        return circuit->latches[offset].lit;
+        break;
+
+    case CIRCUIT_TYPE::OUTPUT:
+        return circuit->outputs[offset].lit;
+        break;
     
+    default:
+        cerr << "Unknown circuit type" << endl;
+        exit(0);
+    }
+}
+
+const char *get_var_name(unsigned var, int i)
+{
+    string s = to_string(var) + "_" + to_string(0);
+    return s.c_str();
+}
+
+aalta_formula *get_var_af(unsigned var, int i)
+{
+    return aalta_formula(get_var_name(var, i)).unique();
+}
+
+void add_equivalence(aalta_formula *lhs, aalta_formula *rhs)
+{
+    // Minisat lit or aalta_formula ???
+}
+
+void add_and_gate(aalta_formula *lhs, aalta_formula *rhs0, aalta_formula *rhs1)
+{
+    aalta_formula *and_rhs_af = aalta_formula(aalta_formula::And, rhs0, rhs1).unique();
+    add_equivalence(lhs, and_rhs_af);
+}
+
+void add_latch(aalta_formula *init, aalta_formula *next)
+{
+    aalta_formula *next_rhs_af = aalta_formula(aalta_formula::Next, NULL, next).unique();
+    add_equivalence(init, next_rhs_af);
+}
+
+bool check_SAT(aalta_formula *af);
+bool check_valid(aalta_formula *af)
+{
+    aalta_formula *not_af = af_not(af);
+    return !check_SAT(not_af);
+}
+
+int main()
+{
+    const char *aigerFile = "example.aag";
+
     // Load AIGER file
-    aiger* circuit = aiger_init();
+    aiger *circuit = aiger_init();
     auto result = aiger_open_and_read_from_file(circuit, aigerFile);
-    if (result != NULL) {
+    if (result != NULL)
+    {
         std::cerr << "Failed to read AIGER file: " << aiger_error(circuit) << std::endl;
         return 1;
     }
-    
-    // Access circuit information
-    uint64_t numInputs = circuit->num_inputs;
-    uint64_t numOutputs = circuit->num_outputs;
-    uint64_t numLatches = circuit->num_latches;
-    uint64_t numAnds = circuit->num_ands;
-    
+
     // Print circuit information
-    std::cout << "Inputs: " << numInputs << std::endl;
-    std::cout << "Outputs: " << numOutputs << std::endl;
-    std::cout << "Latches: " << numLatches << std::endl;
-    std::cout << "AND gates: " << numAnds << std::endl;
+    std::cout << "Inputs: " << circuit->num_inputs << std::endl;
+    std::cout << "Outputs: " << circuit->num_outputs << std::endl;
+    std::cout << "Latches: " << circuit->num_latches << std::endl;
+    std::cout << "AND gates: " << circuit->num_ands << std::endl;
 
     // Iterate over the AND gates
-    for (unsigned i = 0; i < circuit->num_ands; ++i) {
-        aiger_and* and_gate = circuit->ands + i;
+    for (unsigned i = 0; i < circuit->num_ands; ++i)
+    {
+        aiger_and *and_gate = circuit->ands + i;
         unsigned output_var = and_gate->lhs;
         unsigned input1_var = and_gate->rhs0;
         unsigned input2_var = and_gate->rhs1;
         std::cout << "AND Gate: Output = " << output_var
                   << ", Inputs = " << input1_var << ", " << input2_var << std::endl;
     }
+
     // BMC
-    aalta_formula* formula = aalta_formula::TRUE(); // store the rules
-    for(unsigned j = 0; j< circuit->num_latches; j++){
-        aiger_symbol* latches = circuit->latches + j;
-        unsigned latch_var = latches->lit;
-        aalta_formula* tmp = aalta_formula::not(aalta_formula(to_string(latch_var) + "_" + to_string(0)).unique()).unique();
-        // initial vals(all zeros) of latch_var is also input of the rules
-        formula = aalta_formula::simplify_and(formula,tmp);
+    aalta_formula *input_af = aalta_formula::TRUE(); // store the rules
+    for (unsigned j = 0; j < circuit->num_latches; j++)
+    {
+        unsigned lit = get_lit(circuit, CIRCUIT_TYPE::LATCHE, j);
+        aalta_formula *latch_af = get_var_af(lit, 0);
+
+        // initial vals(all zeros) of latch_var are fixed inputs of our circuit model
+        input_af = aalta_formula(aalta_formula::And, input_af, af_not(latch_af)).unique();
     }
-    for(unsigned i = 0; i<20; i++){
-        
-        for(unsigned j = 0; j< circuit->num_outputs; j++){
-            aiger_symbol* outputs = circuit->outputs + j;
-            unsigned output_var = outputs->lit;
-            unsigned output_gate = output_var %2==1 ? output_var-1:output_var;
-            aalta_formula* tmp = aalta_formula(to_string(output_var) + "_" + to_string(i)).unique();
-            if(output_gate != output_var){
-                tmp = aalta_formula::not(tmp).unique();
-            formula = aalta_formula::simplify_and(formula,tmp);
+    
+    // TODO: add even and odd id var equivalence constraints
+
+    aalta_formula *rule_af = aalta_formula::TRUE();
+    for (unsigned bound_step = 0; bound_step < BOUND_LEN; bound_step++)
+    {
+        for (unsigned i = 0; i < circuit->num_outputs; i++)
+        {
+            unsigned lit = circuit->outputs[i].lit;
+            aalta_formula *output_af = get_var_af(lit, 0);
+
+            // rule: vals of outputs are zeros forever
+            rule_af = aalta_formula(aalta_formula::And, rule_af, af_not(output_af)).unique();
         }
-        unordered_map<int, aalta_formula>latch_map;
 
-        for(unsigned j=0; j < circuit->num_ands; j++){
-            // 处理latch
-            aiger_and* and_gate = circuit->ands + j;
-            unsigned output_var = and_gate->lhs;
-            unsigned input1_var = and_gate->rhs0;
-            unsigned input2_var = and_gate->rhs1;
-            
-            unsigned output_gate = and_gate->lhs%2==1? output_var-1:output_var;
-            unsigned input1_gate = and_gate->rhs0%2==1? input1_var-1:input1_var;
-            unsigned input2_gate = and_gate->rhs1%2==1? input2_var-1:input2_var;
+        for (unsigned i = 0; i < circuit->num_ands; i++)
+        {
+            aiger_and *and_gate = circuit->ands + i;
+            aalta_formula *lhs_af = get_var_af(and_gate->lhs, bound_step);
+            aalta_formula *rhs0_af = get_var_af(and_gate->rhs0, bound_step);
+            aalta_formula *rhs1_af = get_var_af(and_gate->rhs1, bound_step);
 
+            add_and_gate(lhs_af, rhs0_af, rhs1_af);
+        }
 
-            aalta_formula* r1=NULL;
-            aalta_formula* r2=NULL;
-            /* STEP1: 判断与门的值是否出现过，没有则构造 */
-            if(latch_map[input1_gate]){
-                // 如果是非门
-                if(input1_gate!= input1_var){
-                    r1 = aalta_formula::not(latch_map[input1_var]).unique();
-                }else{
-                    r1 = latch_map[input1_var].unique();
-                }
-            }else{
-                if(input1_gate!= input1_var){
-                    r1 = aalta_formula::not(aalta_formula(to_string(input1_var) + "_" + to_string(i)).unique()).unique();
-                }else{
-                    r1 = aalta_formula(to_string(input1_var) + "_" + to_string(i)).unique()
-                }
-            }
-            if(latch_map[input2_gate]){
-                // 如果是非门
-                if(input2_gate!= input2_var){
-                    r1 = aalta_formula::not(latch_map[input2_var]).unique();
-                }else{
-                    r1 = latch_map[input2_var].unique();
-                }
-            }else{
-                if(input2_gate!= input2_var){
-                    r1 = aalta_formula::not(aalta_formula(to_string(input2_var) + "_" + to_string(i)).unique()).unique();
-                }else{
-                    r1 = aalta_formula(to_string(input2_var) + "_" + to_string(i)).unique()
-                }
-            }
-            latch_map[output_var] = temp; // 将新的与门加入map
-            // formula = aalta_formula::simplify_and(aalta_formula::simplify_and(formula,r1).unique(),r2).unique();
+        /* add latch trans formulas !!! */
+        for (unsigned i = 0; i < circuit->num_latches; i++)
+        {
+            unsigned init_lit = circuit->latches[i].lit;
+            unsigned next_lit = circuit->latches[i].next;
+            aalta_formula *init_af = get_var_af(init_lit, bound_step);
+            aalta_formula *next_af = get_var_af(next_lit, bound_step);
 
-            /* STEP2: TODO: add latch trans formulas !!! */
+            add_latch(init_af, next_af);
+        }
 
-            /* STEP3: TODO: construct current BMC formula !!! */
+        /* check current step BMC */
+        if (!check_valid(rule_af))
+        {
+            cout << "BMC fails at bound " << bound_step << endl;
+            break;
         }
     }
+
     // Cleanup
     aiger_reset(circuit);
-    
+
     return 0;
 }
